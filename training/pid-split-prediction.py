5c2ea6979b217eb0491ccbb4d8942413ae1fb167
Paul Hildisch <paul-hildisch@gmx.de>
2026-07-11 07:25:03 +0200

    speed up scaling and predicting

--- training/pid-split-prediction.py
+++ training/pid-split-prediction.py
@@ -3,12 +3,12 @@ import pandas as pd
 import time
 import argparse
 import joblib
-import pickle
+import warnings
+import progressbar
 from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
 from matplotlib import dates as dates
 from sklearn.metrics import r2_score, mean_absolute_error
-import warnings
 
 def evaluate_model(prediction, actual):
     r2 = r2_score(actual, prediction)
@@ -36,12 +36,11 @@ def plot(prediction, actual, range = None, title="L2 Regression - Actual & Predi
 
     _ , ax = plt.subplots(figsize=(10, 5))
     
-    ax.scatter(
+    ax.plot(
         actual.index.values[start:end],
         actual[start:end],
         label="Actual Energy",
-        s=0.2,
-        c='g'
+        linewidth=1.0
     )
     ax.plot(
         prediction.index.values[start:end],
@@ -67,33 +66,35 @@ def plot(prediction, actual, range = None, title="L2 Regression - Actual & Predi
     plt.close()
 
 def predictions(data, model, zero_prediction, actual):
-    print("making predictions")
     preds = pd.DataFrame(0, index=actual.index, columns=actual.columns) 
+    bar = progressbar.ProgressBar(max_value=len(data.groupby(level=0)), widgets=['Making predictions:', ' ', progressbar.Percentage(), ' ', progressbar.Bar('#'), ' ', progressbar.Timer()], redirect_stdout=True)
     for timestamp, interval_df in data.groupby(level=0):
         interval_df = interval_df.droplevel(0)
         interval_pred = 0
-        for pid, pid_df in interval_df.groupby(level=0):
-            prediction = model.predict(pid_df)
-            interval_pred += prediction - zero_prediction
-        #print(f"{timestamp} - Interval Prediction: {interval_pred}\n{timestamp} - Interval + Zero: {zero_prediction +  interval_pred}\n{timestamp} - Recorded Value: {actual.loc[timestamp, "interval_energy"]}")
-        interval_pred += zero_prediction
-        preds.loc[timestamp] = interval_pred
+        predictions = model.predict(interval_df)
+        interval_pred = sum([prediction - zero_prediction for prediction in predictions])
+        # print(interval_pred)
+        # for pid, pid_df in interval_df.groupby(level=0):
+        #     prediction = model.predict(pid_df)
+        #     interval_pred += prediction - zero_prediction
+        # #print(f"{timestamp} - Interval Prediction: {interval_pred}\n{timestamp} - Interval + Zero: {zero_prediction +  interval_pred}\n{timestamp} - Recorded Value: {actual.loc[timestamp, "interval_energy"]}")
+        preds.loc[timestamp] = interval_pred + zero_prediction
+        bar.update(bar.value+1)
+    bar.finish('\n')
     return preds
 
 def read_data(measurementsPath, targetsPath, scaler):
     x = pd.read_parquet(measurementsPath)
     y = pd.read_parquet(targetsPath)
-    print(y.mean().interval_energy)
 
-    print("Scaling data")
     if scaler:
+        bar = progressbar.ProgressBar(max_value=len(x.groupby(level=0)), widgets=['Scaling data:', ' ', progressbar.Percentage(), ' ', progressbar.Bar('#'), ' ', progressbar.Timer()], redirect_stdout=True)
+
         for timestamp, interval_df in x.groupby(level=0):
             interval_df = interval_df.droplevel(0)
-            transformed = scaler.transform(interval_df)
-            transformed_df = pd.DataFrame(data=transformed,
-                                           index=interval_df.index,
-                                           columns=interval_df.columns)
-            x.loc[timestamp].update(transformed_df)
+            x.loc[timestamp].iloc[:,:] = scaler.transform(interval_df)
+            bar.update(bar.value+1)
+        bar.finish('\n')
     else: 
         scaler = StandardScaler()
         x = scaler.fit_transform(x)
@@ -106,7 +107,6 @@ def main(args):
 
     zero_df = pd.DataFrame(scaler.transform(np.zeros((1, 12))))
     zero_prediction = model.predict(zero_df)
-    print(zero_prediction)
     data, actual = read_data(args.pidDataSource, args.targetDataSource, scaler)
 
     prediction = predictions(data, model, zero_prediction, actual)
