bc7c9e4dc1de401a7f0db8c8f012ab2c760b6de0
Johnn-ui2010 <congdungnguyen5@gmail.com>
2026-08-08 23:05:01 +0000

    Added a modified full pipeline for DL.

--- model_testing/clean_impl/pipeline/wrappers_dl.py
+++ model_testing/clean_impl/pipeline/wrappers_dl.py
@@ -2,22 +2,26 @@ import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
 #from time import perf_counter
 
-# Basic Deep Learning with Sklearn MLP
+# Basic Deep Learning
 from sklearn.neural_network import MLPRegressor
 from sklearn.inspection import permutation_importance
 
+from keras import layers, optimizers, callbacks, Sequential,regularizers
+from keras.wrappers import SKLearnRegressor
+
 # MLP Wrapper
 class SafeMLPWrapper(BaseEstimator, RegressorMixin):
-    def __init__(self,activation="relu", solver="adam"):
+    def __init__(self,activation="relu", solver="adam", batch_size=256):
         self.activation = activation
         self.solver = solver
+        self.batch_size = batch_size
         self.model = MLPRegressor(hidden_layer_sizes=(128,32,16),
                             activation='relu',
                             solver='adam',
                             learning_rate_init=0.0001,
                             max_iter=500,
                             #alpha = 0.0000675,
-                            batch_size=256,
+                            batch_size=batch_size,
                             early_stopping=True,    # Crucial for time-series stability
                             #validation_fraction=0.1,
                             random_state=42)
@@ -43,3 +47,43 @@ class SafeMLPWrapper(BaseEstimator, RegressorMixin):
 
     def predict(self, X):
         return self.model.predict(X)
+
+standard_callbacks = [
+    callbacks.TerminateOnNaN(),
+    callbacks.EarlyStopping(monitor='loss',patience=3),
+    ]
+
+class SafeKerasWrapper(RegressorMixin, BaseEstimator):
+    def __init__(self,model = None):
+        self.model = model
+
+    def fit(self, X, y):
+        
+        self.model.compile(optimizer=optimizers.Adam(learning_rate=0.001, epsilon=1e-4), loss='mse', metrics=['mae'])
+        #training_fs_start_time = perf_counter()
+
+        self.model.fit(X, y, epochs=20, batch_size=256, callbacks=standard_callbacks, verbose = 0)
+        #training_fs_end_time = perf_counter()
+        print("permutation_importance:")
+        print(X.shape)
+        print(y.shape)
+
+        all_importances = permutation_importance(self.model, X, y,
+                           n_repeats=5,
+                           scoring='neg_mean_squared_error',
+                           random_state=42,
+                           n_jobs = -1)
+        all_importances = np.array(all_importances.importances_mean)
+        # Now convert to numpy array and slice it for SelectFromModel
+        #print(np.array(all_importances))
+
+        self.feature_importances_ = all_importances
+        #print(self.feature_importances_ )
+        
+        #training_fs_time = training_fs_end_time - training_fs_start_time
+        #print(f"Training feature selection time: {training_fs_time:.2f} seconds")
+        return self
+
+    def predict(self, X):
+        return self.model.predict(X, verbose = 0)
+
